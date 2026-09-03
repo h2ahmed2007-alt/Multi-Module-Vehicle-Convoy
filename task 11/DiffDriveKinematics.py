cdd61189f0f328b8e3d4e7701f92612499e4e951
abdalrahman-elsayed181 <abdelrahman.elsayed7006@gmail.com>
2026-09-03 16:36:28 +0300

    Add diff drive kinematics so names are known

---
+++ task 11/DiffDriveKinematics.py
@@ -0,0 +1,22 @@
+from Kinematics import Kinematics
+
+class DiffDriveKinematics(Kinematics):
+    
+    def __init__(self, L, W ,R):
+        super().__init__(L, W, R)
+        self.M_forward
+        self.M_inverse
+
+
+    def forward(self, w):
+        
+        
+        
+    def inverse(self, vx, vy, wz): 
+
+
+
+
+
+
+
