afa911b4048fdefb601e546d7a1d4e2cade08f72
abdalrahman-elsayed181 <abdelrahman.elsayed7006@gmail.com>
2026-09-03 17:36:27 +0300

    add m_forward matrices& eqns 1

--- task 11/DiffDriveKinematics.py
+++ task 11/DiffDriveKinematics.py
@@ -1,14 +1,32 @@
 from Kinematics import Kinematics
+import numpy as np
 
 class DiffDriveKinematics(Kinematics):
     
     def __init__(self, L, W ,R):
         super().__init__(L, W, R)
-        self.M_forward
-        self.M_inverse
+
+
+
+     ----> # forward matrix coefficients  
+        self.M_forward = np.array([
+            [R/2 ,R/2 ],  
+            [0, 0],
+            [R/L, -R/L]
+        ])
+
+     ----> # inverse matrix coefficients  
+        self.M_inverse = np.array([
+            [1/R ,0 , L/(2*R)],
+            [1/R ,0 , -L/(2*R)]
+
+        ])
 
 
     def forward(self, w):
+        omeage_array = np.array([w[0],w[1]])
+        result = omeage_array @ self.M_forward
+        return result
         
         
         
