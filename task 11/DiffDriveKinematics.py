fbca2fd1a818c70d97fc5c9680270fa372ff4b45
abdalrahman-elsayed181 <abdelrahman.elsayed7006@gmail.com>
2026-09-03 19:17:34 +0300

    Fix errors & write diff drive matrices

--- task 11/DiffDriveKinematics.py
+++ task 11/DiffDriveKinematics.py
@@ -8,14 +8,14 @@ class DiffDriveKinematics(Kinematics):
 
 
 
-     ----> # forward matrix coefficients  
+      # ---->forward matrix coefficients  
         self.M_forward = np.array([
             [R/2 ,R/2 ],  
             [0, 0],
             [R/L, -R/L]
         ])
 
-     ----> # inverse matrix coefficients  
+     # ---->  inverse matrix coefficients  
         self.M_inverse = np.array([
             [1/R ,0 , L/(2*R)],
             [1/R ,0 , -L/(2*R)]
@@ -24,15 +24,16 @@ class DiffDriveKinematics(Kinematics):
 
 
     def forward(self, w):
-        omeage_array = np.array([w[0],w[1]])
-        result = omeage_array @ self.M_forward
-        return result
+        omega_array = np.array([w[0],w[1]])
+        forward_result = self.M_forward @ omega_array 
+        return forward_result
         
         
         
     def inverse(self, vx, vy, wz): 
-
-
+        velocity_array = np.array([vx,vy,wz])
+        inverse_result = self.M_inverse @ omeage_array 
+        return inverse_result
 
 
 
